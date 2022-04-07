eeb2eb17f760b33a9206fa7faab03c1ea202b3fd
Malik City <47469354+mcity42@users.noreply.github.com>
2022-04-07 10:24:43 -0500

    Update destination_city.py

--- destination_city.py
+++ destination_city.py
@@ -18,23 +18,23 @@ def destination_find():
  while curr_season == "" or choice == "" :
   curr_season = input("What season is it?").capitalize()
  
- if curr_season in ["Fall"]:
-  choice = vacation_cities["Fall"][random.randint(0, len(vacation_cities["Fall"]) -1)]
-  print(f"Your next fall vacation destination is {choice}")
+  if curr_season in ["Fall"]:
+   choice = vacation_cities["Fall"][random.randint(0, len(vacation_cities["Fall"]) -1)]
+   print(f"Your next fall vacation destination is {choice}")
   
- elif curr_season in ["Spring"]:
-  choice = vacation_cities["Spring"][random.randint(0, len(vacation_cities["Spring"]) -1)]
-  print(f"Your next spring vacation destination is {choice}")
+  elif curr_season in ["Spring"]:
+   choice = vacation_cities["Spring"][random.randint(0, len(vacation_cities["Spring"]) -1)]
+   print(f"Your next spring vacation destination is {choice}")
   
- elif curr_season == "Winter":
-  choice = vacation_cities["Winter"][random.randint(0, len(vacation_cities["Winter"]) -1)]
-  print(f"Your next winter vacation destination is {choice}")
+  elif curr_season == "Winter":
+   choice = vacation_cities["Winter"][random.randint(0, len(vacation_cities["Winter"]) -1)]
+   print(f"Your next winter vacation destination is {choice}")
  
- elif curr_season == "Summer":
-  choice = vacation_cities["Summer"][random.randint(0, len(vacation_cities["Summer"]) -1)]
-  print(f"Your next summer vacation destination is {choice}")
+  elif curr_season == "Summer":
+   choice = vacation_cities["Summer"][random.randint(0, len(vacation_cities["Summer"]) -1)]
+   print(f"Your next summer vacation destination is {choice}")
   
- else:
-  print("Enter valid season")
+  else:
+   print("Enter valid season")
  
 destination_find()
